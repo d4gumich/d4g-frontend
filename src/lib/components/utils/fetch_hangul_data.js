31bffdb2205d53596e07e2d90a150928575c9b1c
reipask <rpaskvan@umich.edu>
2025-02-23 23:45:46 -0500

    Fix URL in FetchHangul JS

--- src/lib/components/utils/fetch_hangul_data.js
+++ src/lib/components/utils/fetch_hangul_data.js
@@ -13,7 +13,7 @@ export async function fetchSummary(summary_parameters, version) {
   for (let fetch_attempt = 0; fetch_attempt < MAX_RETRIES; fetch_attempt++) {
     try {
       const response = await fetch(
-        `http://127.0.0.1:5000/api/v${version}/products/summary`,
+        `https://d4gumsi.pythonanywhere.com/api/v${version}/products/summary`,
         {
           method: "POST",
           headers: headers_,
@@ -52,7 +52,7 @@ async function fetchData(file, version, timeout) {
 
       const response = await Promise.race([
         fetch(
-          `http://127.0.0.1:5000/api/v${version}/products/hangul`,
+          `http://d4gumsi.pythonanywhere.com/api/v${version}/products/hangul`,
         {
           method: "POST",
           body: form,
