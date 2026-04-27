73a021dc33c88114590827de05cc105604d36f72
Matthew Ziminski <26189380+mziminski@users.noreply.github.com>
2026-04-27 01:04:44 -0500

    fix: exclude svelte-tabs from optimization to silence warning

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({      
 	plugins: [sveltekit()],
   optimizeDeps: {
-    include: ['svelte-tabs'],
+    exclude: ['svelte-tabs'],
     esbuildOptions: {
       target: 'esnext'
     }
